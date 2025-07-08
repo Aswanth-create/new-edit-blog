d1031fa5fd55615627035452953c1e173cbba853
Aswanth-create <kishanaswanth@gmail.com>
2025-07-08 17:25:29 +0530

    Initial commit

--- backend/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://nikunjrenjithrajan:nikunjrr@cluster0.3p6hppk.mongodb.net/blogapp?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error:", err));
